a621078a8756ed4d2624821d224e7cb4c7fd1769
berta00 <bertagnollimarco999@gmail.com>
2022-12-18 14:11:25 +0100

    switch

---
+++ src/static/script/secPage.js
@@ -0,0 +1,20 @@
+switch(sezione){
+    case "modulistica":
+
+        break
+    case "direttivo":
+
+        break;
+    case "allenatori":
+
+        break;
+    case "regolamento":
+
+        break;
+    case "corsiPromo":
+
+        break;
+    case "agonismo":
+
+        break;
+}
